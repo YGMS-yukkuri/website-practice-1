b2f47ca52a9557380b3ba10d7aa5a67d731280c6
yukkuri <reityan.sakura0104@gmail.com>
2026-01-20 11:06:58 +0900

    pointerの変更

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const header = document.querySelector("header")
-const headerBtn = document.querySelector("header button")
+const header = document.querySelector("header");
+const headerBtn = document.querySelector("header button");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY !== 0) {
-        header.classList.add("leave")
-        headerBtn.classList.add("leave")
+        header.classList.add("leave");
+        headerBtn.classList.add("leave");
     } else {
-        header.classList.remove("leave")
-        headerBtn.classList.remove("leave")
-    }
-})
+        header.classList.remove("leave");
+        headerBtn.classList.remove("leave");
+    };
+});

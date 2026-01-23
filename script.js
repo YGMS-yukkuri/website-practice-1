da0b51c2a01ee62644f0c7b9392a385effff176b
yukkuri <reityan.sakura0104@gmail.com>
2026-01-24 00:01:05 +0900

    追加

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const header = document.querySelector("header");
 const headerBtn = document.querySelector("header button");
+const formName = document.querySelector("#name")
+const formEmail = document.querySelector("#mail")
+const formMessage = document.querySelector("#message")
+const formButton = document.querySelector("#formBtn")
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
 const changeHeader = () => {
@@ -11,9 +16,76 @@ const changeHeader = () => {
         headerBtn.classList.remove("leave");
     };
 }
+
+const classMatch = (elem,judge) => {
+    if (judge === "ok") {
+        elem.classList.remove("red")
+    } else {
+        elem.classList.add("red")
+    }
+}
+
+const formCheck = () => {
+    let result = [];
+    if (formName.value) {
+        result.push("ok")
+        classMatch(formName,"ok")
+    } else {
+        result.push("ng")
+        classMatch(formName,"ng")
+    }
+
+    if (emailRegex.test(formEmail.value)) {
+        result.push("ok")
+        classMatch(formEmail,"ok")
+    } else {
+        result.push("ng")
+        classMatch(formEmail,"ng")
+    }
+
+    if (formMessage.value) {
+        result.push("ok")
+        classMatch(formMessage,"ok")
+    } else {
+        result.push("ng")
+        classMatch(formMessage,"ng")
+    }
+
+    return result
+}
+
+const formSend = () => {
+    console.log("OK");
+    
+}
+
 window.addEventListener("scroll", () => {
     changeHeader();
 });
 
 
 changeHeader();
+
+
+
+formButton.addEventListener("click", () => {
+    temp = formCheck()
+    console.log(temp)
+    let error = [];
+    let count
+    temp.forEach((element,idx) => {
+        if (element === "ng") {
+            error.push(idx)
+        }
+        else {
+            count++
+        }
+    });
+
+    console.log(error);
+
+    if (count === 3) {
+        formSend()
+    }
+    
+})
